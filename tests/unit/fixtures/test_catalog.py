bf52c91d042564663dd232944a3e80e2f00fc00b
Cor <jczuurmond@protonmail.com>
2024-09-30 21:16:02 +0200

    Remove references to UCX (#56)
    ## Changes
    Remove references to UCX for fixture names

--- tests/unit/fixtures/test_catalog.py
+++ tests/unit/fixtures/test_catalog.py
@@ -12,18 +12,18 @@ def test_make_table_no_args():
     assert table_info == TableInfo(
         catalog_name='hive_metastore',
         schema_name='dummy_srandom',
-        name='ucx_trandom',
+        name='dummy_trandom',
         table_type=TableType.MANAGED,
         data_source_format=DataSourceFormat.DELTA,
-        full_name='hive_metastore.dummy_srandom.ucx_trandom',
-        storage_location='dbfs:/user/hive/warehouse/dummy_srandom/ucx_trandom',
+        full_name='hive_metastore.dummy_srandom.dummy_trandom',
+        storage_location='dbfs:/user/hive/warehouse/dummy_srandom/dummy_trandom',
         properties={'RemoveAfter': '2024091313'},
     )
 
     assert ctx['sql_backend'].queries == [
         'CREATE SCHEMA hive_metastore.dummy_srandom WITH DBPROPERTIES (RemoveAfter=2024091313)',
-        "CREATE TABLE hive_metastore.dummy_srandom.ucx_trandom (id INT, value STRING) TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
-        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.ucx_trandom',
+        "CREATE TABLE hive_metastore.dummy_srandom.dummy_trandom (id INT, value STRING) TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
+        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.dummy_trandom',
         'DROP SCHEMA IF EXISTS hive_metastore.dummy_srandom CASCADE',
     ]
 
@@ -34,18 +34,18 @@ def test_make_view():
     assert table_info == TableInfo(
         catalog_name='hive_metastore',
         schema_name='dummy_srandom',
-        name='ucx_trandom',
+        name='dummy_trandom',
         table_type=TableType.VIEW,
-        full_name='hive_metastore.dummy_srandom.ucx_trandom',
+        full_name='hive_metastore.dummy_srandom.dummy_trandom',
         properties={'RemoveAfter': '2024091313'},
         view_definition='SELECT 1',
     )
 
     assert ctx['sql_backend'].queries == [
         'CREATE SCHEMA hive_metastore.dummy_srandom WITH DBPROPERTIES (RemoveAfter=2024091313)',
-        "CREATE VIEW hive_metastore.dummy_srandom.ucx_trandom AS SELECT 1",
-        "ALTER VIEW hive_metastore.dummy_srandom.ucx_trandom SET TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
-        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.ucx_trandom',
+        "CREATE VIEW hive_metastore.dummy_srandom.dummy_trandom AS SELECT 1",
+        "ALTER VIEW hive_metastore.dummy_srandom.dummy_trandom SET TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
+        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.dummy_trandom',
         'DROP SCHEMA IF EXISTS hive_metastore.dummy_srandom CASCADE',
     ]
 
@@ -56,27 +56,27 @@ def test_make_external_table():
     assert table_info == TableInfo(
         catalog_name='hive_metastore',
         schema_name='dummy_srandom',
-        name='ucx_trandom',
+        name='dummy_trandom',
         table_type=TableType.EXTERNAL,
         data_source_format=DataSourceFormat.JSON,
-        full_name='hive_metastore.dummy_srandom.ucx_trandom',
-        storage_location='dbfs:/tmp/ucx_test_RANDOM',
+        full_name='hive_metastore.dummy_srandom.dummy_trandom',
+        storage_location='dbfs:/tmp/dummy_trandom',
         properties={'RemoveAfter': '2024091313'},
     )
 
     ctx['log_workspace_link'].assert_called_with(
-        'hive_metastore.dummy_srandom.ucx_trandom schema',
-        'explore/data/hive_metastore/dummy_srandom/ucx_trandom',
+        'hive_metastore.dummy_srandom.dummy_trandom schema',
+        'explore/data/hive_metastore/dummy_srandom/dummy_trandom',
     )
 
     assert ctx['sql_backend'].queries == [
         'CREATE SCHEMA hive_metastore.dummy_srandom WITH DBPROPERTIES (RemoveAfter=2024091313)',
-        'CREATE TABLE hive_metastore.dummy_srandom.ucx_trandom USING json location '
-        "'dbfs:/tmp/ucx_test_RANDOM' as SELECT CAST(calories_burnt AS INT) AS `id`, "
+        'CREATE TABLE hive_metastore.dummy_srandom.dummy_trandom USING json location '
+        "'dbfs:/tmp/dummy_trandom' as SELECT CAST(calories_burnt AS INT) AS `id`, "
         'CAST(device_id AS STRING) AS `value` FROM '
         'JSON.`dbfs:/databricks-datasets/iot-stream/data-device`',
-        "ALTER TABLE hive_metastore.dummy_srandom.ucx_trandom SET TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
-        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.ucx_trandom',
+        "ALTER TABLE hive_metastore.dummy_srandom.dummy_trandom SET TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
+        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.dummy_trandom',
         'DROP SCHEMA IF EXISTS hive_metastore.dummy_srandom CASCADE',
     ]
 
@@ -87,18 +87,18 @@ def test_make_table_custom_schema():
     assert table_info == TableInfo(
         catalog_name='hive_metastore',
         schema_name='dummy_srandom',
-        name='ucx_trandom',
+        name='dummy_trandom',
         table_type=TableType.MANAGED,
         data_source_format=DataSourceFormat.DELTA,
-        full_name='hive_metastore.dummy_srandom.ucx_trandom',
-        storage_location='dbfs:/user/hive/warehouse/dummy_srandom/ucx_trandom',
+        full_name='hive_metastore.dummy_srandom.dummy_trandom',
+        storage_location='dbfs:/user/hive/warehouse/dummy_srandom/dummy_trandom',
         properties={'RemoveAfter': '2024091313'},
     )
 
     assert ctx['sql_backend'].queries == [
         'CREATE SCHEMA hive_metastore.dummy_srandom WITH DBPROPERTIES (RemoveAfter=2024091313)',
-        "CREATE TABLE hive_metastore.dummy_srandom.ucx_trandom (`a` INT, `b` STRING) TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
-        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.ucx_trandom',
+        "CREATE TABLE hive_metastore.dummy_srandom.dummy_trandom (`a` INT, `b` STRING) TBLPROPERTIES ( 'RemoveAfter' = '2024091313' )",
+        'DROP TABLE IF EXISTS hive_metastore.dummy_srandom.dummy_trandom',
         'DROP SCHEMA IF EXISTS hive_metastore.dummy_srandom CASCADE',
     ]
 
@@ -120,15 +120,15 @@ def test_make_udf():
     assert fn_info == FunctionInfo(
         catalog_name='hive_metastore',
         schema_name='dummy_srandom',
-        name='ucx_trandom',
-        full_name='hive_metastore.dummy_srandom.ucx_trandom',
+        name='dummy_frandom',
+        full_name='hive_metastore.dummy_srandom.dummy_frandom',
     )
 
     assert ctx['sql_backend'].queries == [
         'CREATE SCHEMA hive_metastore.dummy_srandom WITH DBPROPERTIES (RemoveAfter=2024091313)',
-        'CREATE FUNCTION hive_metastore.dummy_srandom.ucx_trandom(x INT) RETURNS '
+        'CREATE FUNCTION hive_metastore.dummy_srandom.dummy_frandom(x INT) RETURNS '
         'FLOAT CONTAINS SQL DETERMINISTIC RETURN 0;',
-        'DROP FUNCTION IF EXISTS hive_metastore.dummy_srandom.ucx_trandom',
+        'DROP FUNCTION IF EXISTS hive_metastore.dummy_srandom.dummy_frandom',
         'DROP SCHEMA IF EXISTS hive_metastore.dummy_srandom CASCADE',
     ]
 
