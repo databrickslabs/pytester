357fb184d46b9fdab2f09fd949525ecd95e709e6
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-04 10:31:38 +0200

    Test notebook wiuth path

--- tests/unit/fixtures/test_notebooks.py
+++ tests/unit/fixtures/test_notebooks.py
@@ -1,11 +1,15 @@
 from databricks.labs.pytester.fixtures.notebooks import make_notebook, make_directory, make_repo
 from databricks.labs.pytester.fixtures.unwrap import call_stateful
 
-
 def test_make_notebook_no_args():
     ctx, notebook = call_stateful(make_notebook)
     assert ctx is not None
     assert notebook is not None
+
+
+def test_make_notebook_with_path() -> None:
+    _, notebook = call_stateful(make_notebook, path="test.py")
+    assert notebook.name == "test.py"
 
 
 def test_make_directory_no_args():
