8706ccd6b63dcc8c497962fefb57ab5ce45ae8b4
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-04 15:57:38 +0200

    Add integration test for make file

--- tests/integration/fixtures/test_notebooks.py
+++ tests/integration/fixtures/test_notebooks.py
@@ -5,9 +5,14 @@ logger = logging.getLogger(__name__)
 
 def test_creates_some_notebook(make_notebook) -> None:
     notebook = make_notebook()
-    assert notebook.exists()
-    assert "print(1)" in notebook.read_text()
     assert notebook.is_notebook()
+    assert "print(1)" in notebook.read_text()
+
+
+def test_creates_some_file(make_file) -> None:
+    workspace_path = make_file()
+    assert workspace_path.is_file()
+    assert "print(1)" in workspace_path.read_text()
 
 
 def test_creates_some_folder_with_a_notebook(make_directory, make_notebook):
