171e6f09506cf2c68ef6d17cf39b2d6e1823bb58
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-08 09:09:19 +0200

    Fix type hints

--- tests/unit/fixtures/test_compute.py
+++ tests/unit/fixtures/test_compute.py
@@ -34,33 +34,41 @@ def test_make_job_no_args() -> None:
     ctx, job = call_stateful(make_job)
     assert ctx is not None
     assert job is not None
+    assert job.settings is not None
     assert job.settings.name == "dummy-jRANDOM"
     assert job.settings.tags == {"RemoveAfter": "2024091313"}
-    assert len(job.settings.tasks) == 1
-    assert job.settings.tasks[0].task_key == "RANDOM"
-    assert job.settings.tasks[0].description == "RANDOM"
-    assert job.settings.tasks[0].new_cluster.num_workers == 1
-    assert job.settings.tasks[0].new_cluster.spark_conf is None
-    assert job.settings.tasks[0].libraries is None
-    assert job.settings.tasks[0].timeout_seconds == 0
+    tasks = job.settings.tasks
+    assert isinstance(tasks, list) and len(tasks) == 1
+    assert tasks[0].task_key == "RANDOM"
+    assert tasks[0].description == "RANDOM"
+    assert tasks[0].new_cluster is not None
+    assert tasks[0].new_cluster.num_workers == 1
+    assert tasks[0].new_cluster.spark_conf is None
+    assert tasks[0].libraries is None
+    assert tasks[0].timeout_seconds == 0
 
 
 def test_make_job_with_name() -> None:
     _, job = call_stateful(make_job, name="test")
+    assert job.settings is not None
     assert job.settings.name == "test"
 
 
 def test_make_job_with_path() -> None:
     _, job = call_stateful(make_job, path="test.py")
+    assert job.settings is not None
     tasks = job.settings.tasks
-    assert len(tasks) == 1
+    assert isinstance(tasks, list) and len(tasks) == 1
+    assert tasks[0].notebook_task is not None
     assert tasks[0].notebook_task.notebook_path == "test.py"
 
 
 def test_make_job_with_content() -> None:
     ctx, job = call_stateful(make_job, content="print(2)")
+    assert job.settings is not None
     tasks = job.settings.tasks
-    assert len(tasks) == 1
+    assert isinstance(tasks, list) and len(tasks) == 1
+    assert tasks[0].notebook_task is not None
     workspace_path = WorkspacePath(ctx["ws"], tasks[0].notebook_task.notebook_path)
     assert not workspace_path.suffix  # Notebooks have no suffix
     assert workspace_path.read_text() == "print(2)"
@@ -68,8 +76,9 @@ def test_make_job_with_content() -> None:
 
 def test_make_job_with_spark_python_task() -> None:
     ctx, job = call_stateful(make_job, content="print(3)", task_type=SparkPythonTask)
+    assert job.settings is not None
     tasks = job.settings.tasks
-    assert len(tasks) == 1
+    assert isinstance(tasks, list) and len(tasks) == 1
     assert tasks[0].notebook_task is None
     assert tasks[0].spark_python_task is not None
     workspace_path = WorkspacePath(ctx["ws"], tasks[0].spark_python_task.python_file)
@@ -79,25 +88,31 @@ def test_make_job_with_spark_python_task() -> None:
 
 def test_make_job_with_instance_pool_id() -> None:
     _, job = call_stateful(make_job, instance_pool_id="test")
+    assert job.settings is not None
     tasks = job.settings.tasks
-    assert len(tasks) == 1
+    assert isinstance(tasks, list) and len(tasks) == 1
+    assert tasks[0].new_cluster is not None
     assert tasks[0].new_cluster.instance_pool_id == "test"
 
 
 def test_make_job_with_spark_conf() -> None:
     _, job = call_stateful(make_job, spark_conf={"value": "test"})
+    assert job.settings is not None
     tasks = job.settings.tasks
-    assert len(tasks) == 1
+    assert isinstance(tasks, list) and len(tasks) == 1
+    assert tasks[0].new_cluster is not None
     assert tasks[0].new_cluster.spark_conf == {"value": "test"}
 
 
 def test_make_job_with_tags() -> None:
     _, job = call_stateful(make_job, tags={"value": "test"})
+    assert job.settings is not None
     assert job.settings.tags == {"value": "test", "RemoveAfter": "2024091313"}
 
 
 def test_make_job_with_tasks() -> None:
     _, job = call_stateful(make_job, tasks=["CustomTasks"])
+    assert job.settings is not None
     assert job.settings.tasks == ["CustomTasks"]
 
 
